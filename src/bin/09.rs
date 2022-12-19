52391791c5ac92a22aee5b19d63b7be4809c909e
Feng Yao <feng.yao@deepl.com>
2022-12-20 00:49:16 +0100

    09p2

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -15,114 +15,145 @@ impl Position {
         (self.x - other.x).abs() <= 1 && (self.y - other.y).abs() <= 1
     }
 
-    fn keep_up(&self, other: &Self) -> Self {
-        let mut result = Position { x: 0, y: 0 };
-
+    fn keep_up(&mut self, other: &Self) {
         let diff_x = other.x - self.x;
         let diff_y = other.y - self.y;
 
         if diff_x.abs() > diff_y.abs() {
-            result.y = other.y;
-            result.x = if diff_x > 0 { other.x - 1 } else { other.x + 1 }
+            self.y = other.y;
+            self.x = if diff_x > 0 { other.x - 1 } else { other.x + 1 };
+        } else if diff_x.abs() < diff_y.abs() {
+            self.x = other.x;
+            self.y = if diff_y > 0 { other.y - 1 } else { other.y + 1 };
         } else {
-            result.x = other.x;
-            result.y = if diff_y > 0 { other.y - 1 } else { other.y + 1 }
+            self.x = if diff_x > 0 { other.x - 1 } else { other.x + 1 };
+            self.y = if diff_y > 0 { other.y - 1 } else { other.y + 1 };
+            // println!("diff {}, {}", diff_x, diff_y);
         }
-
-        result
     }
 }
 
 struct Rope {
-    head: Position,
-    tail: Position,
+    // head: Position,
+    // tail: Position,
+    knots: Vec<Position>,
     tail_visited: HashSet<Position>,
 }
 
 impl Rope {
-    fn init() -> Self {
+    fn init(knots_count: usize) -> Self {
         let mut tail_visited = HashSet::new();
         tail_visited.insert(Position { x: 0, y: 0 });
 
+        let mut knots: Vec<Position> = Vec::with_capacity(knots_count);
+        for _i in 0..knots_count {
+            knots.push(Position { x: 0, y: 0 });
+        }
+
         Rope {
-            head: Position { x: 0, y: 0 },
-            tail: Position { x: 0, y: 0 },
+            knots: knots,
             tail_visited: tail_visited,
         }
     }
 
+    fn read_input(&mut self, input: &str) {
+        for line in input.lines() {
+            let mut iter = line.split(' ');
+            let command = iter.next().unwrap();
+            let steps: usize = iter.next().unwrap().parse().unwrap();
+
+            // println!("{:?} {:?}", command, steps);
+
+            match command {
+                "L" => self.move_left(steps),
+                "R" => self.move_right(steps),
+                "U" => self.move_up(steps),
+                "D" => self.move_down(steps),
+                _ => println!("Unknown command: {:?}", command),
+            }
+
+            // println!("{:?}", self.knots);
+        }
+    }
+
+    fn head(&mut self) -> &mut Position {
+        self.knots.first_mut().unwrap()
+    }
+
     fn post_move(&mut self) {
         // calculate the position of head and tail, then try to follow
-        if self.head.is_touching(&self.tail) {
-            // println!("{:?} and {:?} is touched", self.head, self.tail);
-            return;
+        let mut n = 1;
+        let len = self.knots.len();
+        // println!("len is {}", len);
+
+        loop {
+            if n > (len - 1) {
+                break;
+            }
+
+            let first = self.knots.get(n - 1).unwrap().clone();
+            let second = self.knots.get_mut(n).unwrap();
+
+            if !first.is_touching(second) {
+                second.keep_up(&first);
+                if n == (len - 1) {
+                    println!("tail {:?} visit {:?}", n, second.clone());
+                    self.tail_visited.insert(second.clone());
+                }
+            }
+
+            n += 1;
         }
-
-        let new_tail = self.tail.keep_up(&self.head);
-        self.tail_visited.insert(new_tail.clone());
-        self.tail = new_tail;
-
-        println!("tail moved to {:?}", &self.tail);
     }
 
     fn move_left(&mut self, times: usize) {
         for _i in 0..times {
-            self.head.x -= 1;
+            self.head().x -= 1;
             self.post_move();
         }
     }
 
     fn move_right(&mut self, times: usize) {
         for _i in 0..times {
-            self.head.x += 1;
+            self.head().x += 1;
             self.post_move();
         }
     }
 
     fn move_up(&mut self, times: usize) {
         for _i in 0..times {
-            self.head.y += 1;
+            self.head().y += 1;
             self.post_move();
         }
     }
 
     fn move_down(&mut self, times: usize) {
         for _i in 0..times {
-            self.head.y -= 1;
+            self.head().y -= 1;
             self.post_move();
         }
     }
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let mut rope = Rope::init();
-
-    for line in input.lines() {
-        let mut iter = line.split(' ');
-        let command = iter.next().unwrap();
-        let steps: usize = iter.next().unwrap().parse().unwrap();
-
-        println!("{:?} {:?}", command, steps);
-
-        match command {
-            "L" => rope.move_left(steps),
-            "R" => rope.move_right(steps),
-            "U" => rope.move_up(steps),
-            "D" => rope.move_down(steps),
-            _ => println!("Unknown command: {:?}", command),
-        }
-    }
-
+    let mut rope = Rope::init(2);
+    // println!("Knots: {:?}", rope.knots);
+    rope.read_input(input);
     Some(rope.tail_visited.len() as u32)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut rope = Rope::init(10);
+    rope.read_input(input);
+    Some(rope.tail_visited.len() as u32)
 }
 
 fn main() {
     let input = &advent_of_code::read_file("inputs", 9);
+    // 6470
     advent_of_code::solve!(1, part_one, input);
+
+    // 2616 too low, 2658
     advent_of_code::solve!(2, part_two, input);
 }
 
@@ -138,7 +169,20 @@ mod tests {
 
     #[test]
     fn test_part_two() {
+        let input = String::from(
+            "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20",
+        );
+
+        assert_eq!(part_two(&input), Some(36));
+
         let input = advent_of_code::read_file("examples", 9);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(1));
     }
 }
